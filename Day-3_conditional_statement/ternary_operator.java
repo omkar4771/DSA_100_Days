fad65a7ed1e1d931cccbdba10695bb6b003f6816
omkar4771 <omkardaswadkar123@gmail.com>
2024-07-22 22:40:02 +0530

    ternary operator

---
+++ Day-3_conditional_statement/ternary_operator.java
@@ -0,0 +1,13 @@
+// package Day-3_conditional_statement;
+
+public class ternary_operator {
+    public static void main(String[] args) {
+        int num = 4;
+         
+        //ternary operator
+
+        String type = ((num%2)==0)? "even" : "odd";
+
+        System.out.println(type);
+    }
+}
