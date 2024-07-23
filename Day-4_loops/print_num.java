f9fadb00bd8d62df43780edd1449e8b79e089aca
omkar4771 <omkardaswadkar123@gmail.com>
2024-07-23 20:21:22 +0530

    print number

---
+++ Day-4_loops/print_num.java
@@ -0,0 +1,12 @@
+// package Day-4_loops;
+
+public class print_num {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while(num<=10){
+            System.out.println(num);
+            num ++;
+        }
+    }
+}
