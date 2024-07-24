7e1dc273c757f4d1bb206809187839c4f5c5e294
omkar4771 <omkardaswadkar123@gmail.com>
2024-07-24 22:19:32 +0530

    star pattern

---
+++ Day-5_patterns_1/starpattern.java
@@ -0,0 +1,12 @@
+// package Day-5_patterns_1;
+
+public class starpattern {
+    public static void main(String[] args) {
+        for(int line=1;line<=5;line++){
+            for(int star=1;star<=line;star++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
