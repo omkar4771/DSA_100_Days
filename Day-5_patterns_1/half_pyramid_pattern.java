1912a5cff7423b420fdd95509233dd02e3f7928f
omkar4771 <omkardaswadkar123@gmail.com>
2024-07-24 22:18:48 +0530

    half pyramid pattern

---
+++ Day-5_patterns_1/half_pyramid_pattern.java
@@ -0,0 +1,12 @@
+// package Day-5_patterns_1;
+
+public class half_pyramid_pattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}   
