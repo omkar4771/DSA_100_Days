6c9fdb38de623289fa7c7ce8fff40ea90f1710c4
omkar4771 <omkardaswadkar123@gmail.com>
2024-07-25 23:03:48 +0530

    even number

---
+++ Day-6_function_and_method/even_num.java
@@ -0,0 +1,12 @@
+public class even_num {
+    public static boolean isEven(int n){
+        if(n%2==0){
+            return true;
+        }
+        return false;
+    }
+ public static void main(String[] args) {
+    int n=4;
+    System.out.println(isEven(n));
+ }   
+}
